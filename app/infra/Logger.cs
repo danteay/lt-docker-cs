68299ae966bd754046cad9bc8d01bf7a799bed96
Eduardo Aguilar <dante.aguilar41@gmail.com>
2025-02-28 22:50:13 -0600

    style: fix lint

--- app/infra/Logger.cs
+++ app/infra/Logger.cs
@@ -7,10 +7,8 @@ namespace app.infra
 {
     public class Logger
     {
-        private readonly static Lazy<Logger> Instance = new Lazy<Logger>(
-            () => new Logger()
-        );
-        
+        private static readonly Lazy<Logger> Instance = new Lazy<Logger>(() => new Logger());
+
         public static Logger GetInstance => Instance.Value;
 
         private Logger() { }
@@ -20,16 +18,19 @@ namespace app.infra
             var level = Environment.GetEnvironmentVariable("LOG_LEVEL") ?? "INFO";
             var secretName = Environment.GetEnvironmentVariable("LOGSTASH_CONFIGS") ?? "";
             var stage = Configuration.GetInstance.Get("STAGE");
-            
+
             var minLevel = level switch
             {
                 "DEBUG" => LogEventLevel.Debug,
                 "ERROR" => LogEventLevel.Error,
                 "WARNING" => LogEventLevel.Warning,
-                _ => LogEventLevel.Information
+                _ => LogEventLevel.Information,
             };
-            
-            var formatter = new ElasticsearchJsonFormatter(renderMessageTemplate: false, inlineFields: true);
+
+            var formatter = new ElasticsearchJsonFormatter(
+                renderMessageTemplate: false,
+                inlineFields: true
+            );
 
             var newLogger = new LoggerConfiguration()
                 .Enrich.FromLogContext()
@@ -37,26 +38,29 @@ namespace app.infra
                 .Enrich.WithProperty("serviceName", Configuration.GetInstance.Get("SERVICE_NAME"))
                 .MinimumLevel.Is(minLevel)
                 .WriteTo.Console(formatter);
-            
+
             if (Configuration.GetInstance.Get("STAGE") != "local")
             {
                 // Execution inside container
                 var secrets = SecretsManager.GetInstance(secretName);
                 secrets.Initialize();
-                
+
                 var elasticHost = secrets.MustGet("ELASTIC_HOST");
                 var elasticUser = secrets.MustGet("ELASTIC_USER");
                 var elasticPassword = secrets.MustGet("ELASTIC_PASSWORD");
-                
-                newLogger.WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(elasticHost))
-                {
-                    AutoRegisterTemplate = true,
-                    AutoRegisterTemplateVersion = AutoRegisterTemplateVersion.ESv8,
-                    IndexFormat = $"logs-{DateTime.UtcNow:yyyy.MM.dd}",
-                    ModifyConnectionSettings = x => x.BasicAuthentication(elasticUser, elasticPassword)
-                });
+
+                newLogger.WriteTo.Elasticsearch(
+                    new ElasticsearchSinkOptions(new Uri(elasticHost))
+                    {
+                        AutoRegisterTemplate = true,
+                        AutoRegisterTemplateVersion = AutoRegisterTemplateVersion.ESv8,
+                        IndexFormat = $"logs-{DateTime.UtcNow:yyyy.MM.dd}",
+                        ModifyConnectionSettings = x =>
+                            x.BasicAuthentication(elasticUser, elasticPassword),
+                    }
+                );
             }
-            
+
             Log.Logger = newLogger.CreateLogger();
         }
     }
